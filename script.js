d4f099459e20f3754656caf50a48ec656c4a5b7a
bcannon95 <brock.cannon@hotmail.com>
2022-03-26 10:11:58 +1100

    Project Complete

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-let movies = ['whiplash', 'psycho', 'kimi', 'shawshank redemption', 'hatter']
+let movies = ['the shawshank redemption','the godfather','the godfather part ii','the dark knight','twelve angry men','schindlers list','pulp fiction','the good the bad and the ugly','the lord of the rings the return of the king','fight club','the lord of the rings: the fellowship of the ring','star wars episode v the empire strikes back','forrest gump','inception','one flew over the cuckoos nest','the lord of the rings: the two towers','goodfellas','the matrix','star wars','seven samurai','city of god','se7en','the silence of the lambs','the usual suspects','its a wonderful life','life is beautiful','léon the professional','once upon a time in the west','interstellar','saving private ryan','american history x','spirited away','casablanca','raiders of the lost ark','psycho','city lights','rear window','the intouchables','modern times','terminator 2 judgment day','whiplash','the green mile','the pianist','memento','the departed','gladiator','apocalypse now','back to the future','sunset blvd','dr strangelove or how i learned to stop worrying and love the bomb','the prestige','alien','the lion king','the lives of others','the great dictator','inside out','cinema paradiso','the shining','paths of glory','django unchained','the dark knight rises','wall e','american beauty','grave of the fireflies','aliens','citizen kane','north by northwest','princess mononoke','vertigo','oldboy','das boot','m','star wars episode vi return of the jedi','once upon a time in america','amelie','witness for the prosecution','reservoir dogs','braveheart','toy story 3','a clockwork orange','double indemnity','taxi driver','requiem for a dream','to kill a mockingbird','lawrence of arabia','eternal sunshine of the spotless mind','full metal jacket','the sting','amadeus','bicycle thieves','singin in the rain','monty python and the holy grail','snatch','2001 a space odyssey','the kid','l.a. confidential','rashomon','for a few dollars more','toy story','the apartment','inglourious bastards','all about eve','the treasure of the sierra madre','jodaeiye nader az simin','indiana jones and the last crusade','metropolis','yojimbo','the third man','batman begins','scarface','some like it hot','unforgiven','3 idiots','up','raging bull','downfall','mad max fury road','jagten','chinatown','the great escape','die hard','good will hunting','heat','on the waterfront','pans labyrinth','mr smith goes to washington','the bridge on the river kwai','my neighbor totoro','ran','the gold rush','ikiru','the seventh seal','blade runner','the secret in their eyes','wild strawberries','the general','lock, stock and two smoking barrels','the elephant man','casino','the wolf of wall street','howls moving castle','warrior','gran torino','v for vendetta','the big lebowski','rebecca','judgment at nuremberg','a beautiful mind','cool hand luke','the deer hunter','how to train your dragon','gone with the wind','fargo','trainspotting','it happened one night','dial m for murder','into the wild','gone girl','the sixth sense','rush','finding nemo','the maltese falcon','mary and max','no country for old men','the thing','incendies','hotel rwanda','kill bill vol 1','life of brian','platoon','the wages of fear','butch cassidy and the sundance kid','there will be blood','network','touch of evil','the 400 blows','stand by me','12 years a slave','the princess bride','annie hall','persona','the grand budapest hotel','amores perros','in the name of the father','million dollar baby','ben-hur','the grapes of wrath','hachi a dogs tale','nausicaa of the valley of the wind','shutter island','diabolique','sin city','the wizard of oz','gandhi','stalker','the bourne ultimatum','the best years of our lives','donnie darko','relatos salvajes','8 and a half','strangers on a train','jurassic park','the avengers','before sunrise','twelve monkeys','the terminator','infernal affairs','jaws','the battle of algiers','groundhog day','memories of murder','guardians of the galaxy','monsters inc','harry potter and the deathly hallows part 2','throne of blood','the truman show','fanny and alexander','barry lyndon','rocky','dog day afternoon','the imitation game','yip man','the kings speech','high noon','la haine','a fistful of dollars','pirates of the caribbean the curse of the black pearl','notorious','castle in the sky','prisoners','the help','whos afraid of virginia woolf','roman holiday','spring summer fall winter and spring','the night of the hunter','beauty and the beast','la strada','papillon','xmen days of future past','before sunset','anatomy of a murder','the hustler','the graduate','the big sleep','underground','elite squad the enemy within','gangs of wasseypur','lagaan once upon a time in india','paris texas','akira']
 let rightGuessString = movies[Math.floor(Math.random() * movies.length)]
 let guess = []
 let nextLetter = 0
 let totalBoxes = rightGuessString.length
-let totalGuesses = Math.floor(Math.random() * 4) + 4;
+let totalGuesses = Math.floor(Math.random() * 8) + 4;
 let currentGuesses = 0;
 console.log(totalGuesses);
 console.log(rightGuessString);
 
 // Creates the gameboard based on the length of the movie to be guessed
 
-function boardDisplay() {
+function createBoard() {
     let board = document.getElementById("game-board");
 
     for (let r = 0; r < totalGuesses; r++) {
@@ -27,15 +27,11 @@ function boardDisplay() {
     }
 }
 
-boardDisplay()
+createBoard()
 
 // Pickups what key was selected and runs from the functions below
 
 document.addEventListener("keyup", function(letter) {
-
-    if (totalGuesses === 0) {
-        return
-    }
 
     let pressedKey = String(letter.key)
     console.log(pressedKey);
@@ -52,13 +48,31 @@ document.addEventListener("keyup", function(letter) {
     let found = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," "].includes(pressedKey)
     console.log(found);
     if (found) {
-         insertLetter(pressedKey)
+         addLetter(pressedKey)
     }
 })
 
+// What was pressed on the virtual keyboard
+
+document.getElementById("keyboard").addEventListener("click", (e) => {
+    const target = e.target
+    
+    if (!target.classList.contains("keys")) {
+        return
+    }
+    let key = target.textContent
+
+    if (key === "Del") {
+        key = "Backspace"
+    } 
+
+    document.dispatchEvent(new KeyboardEvent("keyup", {'key': key}))
+})
+
+
 // Adds a letter to the gameboard
 
-function insertLetter (pressedKey) {
+function addLetter (pressedKey) {
     if (nextLetter === totalBoxes) {
         return
     }
@@ -101,12 +115,6 @@ function checkGuess () {
         alert("Not enough letters!")
         return
     }
-    // Checks if it's a valid movie from the list
-    if (!movies.includes(guessString)) {
-        alert("Word not in list!")
-        return
-    }
-
     
     for (let i = 0; i < totalBoxes; i++) {
         let letterColor = ''
@@ -118,14 +126,10 @@ function checkGuess () {
         if (letterPosition === -1) {
             letterColor = 'grey'
         } else {
-            // now, letter is definitely in word
-            // if letter index and right guess index are the same
-            // letter is in the right position 
             if (guess[i] === rightGuess[i]) {
-                // shade green 
+
                 letterColor = 'green'
             } else {
-                // shade box yellow
                 letterColor = 'yellow'
             }
 
@@ -141,11 +145,10 @@ function checkGuess () {
     }
 
     if (guessString === rightGuessString) {
-        alert("You guessed right! Game over!")
         guessesRemaining = 0
         return
     } else {
-        guessesRemaining -= 1;
+        currentGuesses++;
         guess = [];
         nextLetter = 0;
 
@@ -155,3 +158,5 @@ function checkGuess () {
         }
     }
 }
+
+
